0e4acfe886d0834c87a353e470c6aa96f6f45315
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2022-05-19 02:36:31 -0300

    cart add and delete

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -14,9 +14,18 @@ const useInitialState = () => {
             cart: [...state.cart, payload]
         });
     };
+
+    const removeFromCart = (payload) => {
+        setState({
+            ...state,
+            cart: state.cart.filter(item => item.id 
+                !== payload.id)
+        });
+    }
  return {
      state,
-     addToCart
+     addToCart,
+     removeFromCart,
  }
 }
 
